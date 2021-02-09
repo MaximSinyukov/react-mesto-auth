dc723a02b33835bc7ad93f8a9e058c1e895a17b4
Maxim Sinyukov <verero1173@yandex.ru>
2021-02-09 14:41:32 +0300

    feat: создать функциональность для кнопок хэдера

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import logo from '../images/header__logo.svg';
 
-function Header({ email, onSignOut }) {
+function Header({ email, loggedIn, onSignOut }) {
+  const [isMobileButtonOpen, setIsMobileButtonOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    aa();
+  }, [loggedIn]);
+
+  function aa() {
+    setIsMobileButtonOpen(false);
+  }
+
+  function changeMobileButton() {
+    const value = !isMobileButtonOpen;
+    setIsMobileButtonOpen(value);
+  }
+
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Место" />
-      <p className="header__mail">{(email !== 'Войти') ? ((email !== 'Регистрация') ? email : '') : ''}</p>
-      <button className="header__button" onClick={onSignOut}>{(email === 'Войти') || (email === 'Регистрация') ? email : 'Выйти'}</button>
-      <button className="header__mobile-button"></button>
+      <div className={`header__container ${(!loggedIn)||(isMobileButtonOpen) ? 'header__container_visible' : ''} ${isMobileButtonOpen ? 'header__container_open' : ''}`}>
+        <p className={`header__mail ${(loggedIn)&&(isMobileButtonOpen) ? 'header__mail_visible' : ''}`}>{(email !== 'Войти') ? ((email !== 'Регистрация') ? email : '') : ''}</p>
+        <button className={`header__button ${isMobileButtonOpen ? 'header__button_open' : ''}`} onClick={onSignOut}>{(email === 'Войти') || (email === 'Регистрация') ? email : 'Выйти'}</button>
+      </div>
+      <button className={`header__mobile-button ${loggedIn ? 'header__mobile-button_visible' : ''} ${isMobileButtonOpen ? 'header__mobile-button_open' : ''}`} onClick={changeMobileButton}></button>
     </header>
   );
 }
